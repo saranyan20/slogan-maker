2be41ae02a6aa035ae0a91ae36978becf0f8f6d9
saranyan20 <srisaran2000@gmail.com>
2022-03-30 16:09:05 +0530

    project folder structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello</h1>);
