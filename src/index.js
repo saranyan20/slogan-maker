a05ceaa044e4732f940598a2f91f6e18b5c18ddd
saranyan20 <srisaran2000@gmail.com>
2022-03-31 11:22:03 +0530

    added store with static data added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import './index.css';
+import App from './App';
+import StoreProvider from './Store/store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'font-awesome/css/font-awesome.min.css';
+// import 'node_modules/font-awesome/css/font-awesome.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello</h1>);
+root.render(
+  <StoreProvider>
+    <App />
+  </StoreProvider>
+);
