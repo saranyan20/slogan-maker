dfd55f4662dd20171f4eb743c832afdc75ad6731
saranyan20 <srisaran2000@gmail.com>
2022-03-31 16:46:13 +0530

    added other product section

---
+++ src/Components/OtherProductsSection/OtherProductsSection.jsx
@@ -0,0 +1,47 @@
+import './otherProductsSection.scss';
+import PrivacyPolicyGeneratorImage from '../../Assets/Images/privacy-policy-generator.svg';
+import TermsConditionsGenerator from '../../Assets/Images/terms-conditions-generator.svg';
+import DomainNameGeneratorImage from '../../Assets/Images/domain-name-generator.svg';
+import InvoiceGeneratorImage from '../../Assets/Images/invoice-generator.svg';
+
+function OtherProductsSection() {
+  return (
+    <div className='d-product'>
+      <div className='d-container'>
+        <h5 className='d-product__heading'>Try our other free products</h5>
+        <div className='row my-5'>
+          <div className='col-sm-4 mb-3'>
+            <img src={PrivacyPolicyGeneratorImage} />
+            <h5 className='d-product__title mt-3 mb-2'>Privacy Policy Generator</h5>
+            <p className='d-product__description'>
+              Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.
+            </p>
+          </div>
+          <div className='col-sm-4 mb-3'>
+            <img src={TermsConditionsGenerator} />
+            <h5 className='d-product__title mt-3 mb-2'>Terms & Conditions Generator</h5>
+            <p className='d-product__description'>
+              Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.
+            </p>
+          </div>
+          <div className='col-sm-4 mb-3'>
+            <img src={DomainNameGeneratorImage} />
+            <h5 className='d-product__title mt-3 mb-2'>Domain Name Generator</h5>
+            <p className='d-product__description'>
+              Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.
+            </p>
+          </div>
+          <div className='col-sm-4 mb-3'>
+            <img src={InvoiceGeneratorImage} />
+            <h5 className='d-product__title mt-3 mb-2'>Invoice Generator</h5>
+            <p className='d-product__description'>
+              Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OtherProductsSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,18 @@
 import Header from '../Components/Header/Header';
-import Footer from '../Components/Footer/Footer';
 import SloganMaker from '../Components/SloganMaker/SloganMaker';
 import FeaturesSection from '../Components/FeaturesSection/FeaturesSection';
+import OtherProductsSection from '../Components/OtherProductsSection/OtherProductsSection';
+import Footer from '../Components/Footer/Footer';
 
 function Home() {
   return (
-    <div>
+    <>
       <Header />
       <SloganMaker />
       <FeaturesSection />
+      <OtherProductsSection />
       <Footer />
-    </div>
+    </>
   );
 }
 
