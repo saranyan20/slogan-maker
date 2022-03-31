8e232087aa0af89d5da804717957c8bd6d2dc3aa
saranyan20 <srisaran2000@gmail.com>
2022-03-31 12:29:17 +0530

    added feature section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import Header from '../Components/Header/Header';
 import Footer from '../Components/Footer/Footer';
 import SloganMaker from '../Components/SloganMaker/SloganMaker';
+import FeaturesSection from '../Components/FeaturesSection/FeaturesSection';
 
 function Home() {
   return (
     <div>
       <Header />
       <SloganMaker />
+      <FeaturesSection />
       <Footer />
     </div>
   );
